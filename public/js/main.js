db70471a23c9e277b91f3b9003217492bff4b700
Nicholas Tassone <ntassone@gmail.com>
2014-04-25 15:07:11 -0300

    more content, responsive styling

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,14 @@ $(function() {
   //Menu Toggle
   var $menu = $('.main-nav ul'),
       $menulink = $('.menu-toggle');
-  
+
   $menulink.click(function() {
     $menulink.toggleClass('active');
     $menu.toggleClass('active');
     return false;
   });
+
+  $('.sign-up input[type="email"]').select();
 
 /* Clipboard functionality */
 
