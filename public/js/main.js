b3f470262d4ab0d1847e944641fb7997f1537d88
Nicholas Tassone <ntassone@gmail.com>
2014-04-25 23:08:36 -0300

    added fonts, fixed icons, made a fade

--- public/js/main.js
+++ public/js/main.js
@@ -4,47 +4,10 @@
 $(function() {
   'use strict';
 
-  //Menu Toggle
-  var $menu = $('.main-nav ul'),
-      $menulink = $('.menu-toggle');
-
-  $menulink.click(function() {
-    $menulink.toggleClass('active');
-    $menu.toggleClass('active');
-    return false;
-  });
-
   $('.sign-up input[type="email"]').select();
 
-/* Clipboard functionality */
-
-  var _ = window._;
-  var $clipboard = $('.copy-code-snip');
-  var ZeroClipboard = window.ZeroClipboard;
-
-  var clipper = new ZeroClipboard($clipboard, {
-    moviePath: 'js/vendor/ZeroClipboard.swf'
+  setTimeout(function() {
+    $('body').removeClass('preload');
   });
-
-  clipper.on('complete', function() {
-    var self = this;
-    $(self).addClass('copied');
-    setTimeout(function() {
-      $(self).removeClass('copied');
-    }, 1000);
-  });
-
-  clipper.on('noflash', function() {
-    $clipboard.each(function() {
-      var newEl = this.cloneNode(true);
-      this.parentNode.replaceChild(newEl, this);
-    });
-    $clipboard.text('Select all').click(function() {
-      $(this).prev()[0].select();
-    });
-  });
-
-  $(window).resize(clipper.reposition);
-
 
 });
